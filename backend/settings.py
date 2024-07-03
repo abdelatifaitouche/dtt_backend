6e0c1aa3c6e5da34069139477219791b6d32ba50
abdelatif aitouche <abdelatif.aitouche3@gmail.com>
2024-07-03 19:08:09 +0200

    first deployement : added the allowed hosts

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4$zab8g-@^6&ebjn-4otm&h@w=cttwoxb-lzn7gkf%q5y&veh!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
