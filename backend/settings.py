b3c74664201b3ee5cd0d301a7b71ad711fb065c0
abdelatif aitouche <abdelatif.aitouche3@gmail.com>
2024-07-28 21:56:10 +0200

    added the dividendes api : db changed

--- backend/settings.py
+++ backend/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -99,7 +99,7 @@ DATABASES = {
         'PORT' : '54317'
     }
 }
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
